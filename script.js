197823a21ab8e0c047e6210fc4312489be708020
agcontr1 <alexander.ga.contreras@gmail.com>
2023-06-11 00:35:45 -0700

    query selector

---
+++ script.js
@@ -0,0 +1,12 @@
+var $searchForm = document.querySelector("#city-search");
+var $searchInput = document.querySelector("#search-input");
+var $weatherCard = document.querySelector("#current-weather");
+var $weatherBody = document.querySelector("#weather-body");
+var $forecastSection = document.querySelector("#forecast-section");
+var $fivedayDiv = document.querySelector("#five-day");
+var $historyCard = document.querySelector("#history-card");
+var $searchHistory = document.querySelector("#search-history");
+var $errorMsg = document.querySelector("#error-message");
+
+var APIkey = "5f627cd76ea4710ec0c37266c24bd00f";
+var urlStart = "https://api.openweathermap.org/data/2.5/";
